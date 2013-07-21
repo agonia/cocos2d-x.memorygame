b69530e7d1eb4c71ee29a328e782ee96799e62b9
hwani5 <hwani5@hwani5ui-MacBook-Pro.local>
2013-07-21 18:53:36 +0900

    Initial Commit

---
+++ cocos2d-x.memorygame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  cocos2d_x_memorygameAppController.h
+//  cocos2d-x.memorygame
+//
+//  Created by hwani5 on 13. 7. 21..
+//  Copyright __MyCompanyName__ 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
